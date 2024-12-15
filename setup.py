574d5b89df937f68e892c42c8deba77a65a603e4
孙士川 <sunshichuan@sunshichuandeMacBook-Pro.local>
2024-12-15 20:56:12 +0800

    Update commit for svxyz project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svxyz",
-    version="2.1.2",
+    version="2.1.3",
     description="A suite of scripts for XYZ data processing (txyz, xyz2pos, dxyz, pxyz)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
